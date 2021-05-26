73933c7d017c9f2be477d4200f797a1859243081
thuy98vh <giusethuy98no1@gmail.com>
2021-05-26 17:34:52 +0700

    cach giai tim so chan

--- timsochan.js
+++ timsochan.js
@@ -1,4 +1,15 @@
 // in ra mảng các số chẵn từ dãy sau
 var arr = [1, 4, 32, 4, 7, 9, 20, 21, 61, 72];
-//tut ret
-//thuy that su ngu ngoc ret ret
+function timsochan(arr) {
+    var sochan=[]
+    for(var i=0; i<arr.length; i++) {
+        var a = arr[i]
+        if(a % 2 == 0) {
+            sochan.push(a)
+            
+        } 
+        
+    }
+    console.log(sochan)
+}
+timsochan(arr)
