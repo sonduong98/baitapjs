1e45ff392a4f7e9a82331735c76b5c0bf3b663c3
Duong <sonduong200198@gmail.com>
2021-05-26 15:43:06 +0700

    cach giai bai tinh tong

--- tinhtong.js
+++ tinhtong.js
@@ -1,1 +1,13 @@
 //Đề bài: tính tổng Sn=1/2+3/4+5/6+...+2n+1/2n+2
+function tinhtong(n) {
+  if (n < 0) {
+    console.log("Số truyền vào không thỏa mãn!");
+  } else {
+    let tong = 0;
+    for (let i = 0; i <= n; i++) {
+      tong += (2 * i + 1) / (2 * i + 2);
+    }
+    return tong;
+  }
+}
+console.log(tinhtong(2));
