0d99232375f46d10d4bd913378caa9c0b36b23e6
Duong <sonduong200198@gmail.com>
2021-05-26 17:31:25 +0700

    cach giai bai tim so chan

--- timsochan.js
+++ timsochan.js
@@ -1,4 +1,17 @@
 // in ra mảng các số chẵn từ dãy sau
 var arr = [1, 4, 32, 4, 7, 9, 20, 21, 61, 72];
-//tut ret
-//thuy that su ngu ngoc ret ret
+
+function timsochan(a) {
+  var mang = [];
+  if (a.length > 0) {
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] % 2 == 0) {
+        mang.push(a[i]);
+      }
+    }
+    return mang;
+  } else {
+    return "tham so truyen vao khong thoa man dieu kien";
+  }
+}
+console.log(timsochan(arr));
