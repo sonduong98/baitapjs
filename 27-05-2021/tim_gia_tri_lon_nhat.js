42ac7d1003d69a658a1d1218c4192656118099da
Duong <sonduong200198@gmail.com>
2021-05-27 20:57:08 +0700

    bài tập ngày 27/05

---
+++ 27-05-2021/tim_gia_tri_lon_nhat.js
@@ -0,0 +1,2 @@
+//tính tích các phần tử của mảng sau
+var arr = [99, 69, 96, 24, 36, 98, 101, 198, 20, 16];
---
+++ 27-05-2021/tim_so_chan_lon_nhat.js
@@ -0,0 +1,2 @@
+// tìm và in ra số chẵn lớn nhất trong mảng sau
+var arr = [2, 76, 98, 99, 201, 202, 368, 401, 569];
