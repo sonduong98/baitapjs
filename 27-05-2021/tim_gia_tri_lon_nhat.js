4f37d786853022c3e186568ffe0792b4b90e448e
thuy98vh <giusethuy98no1@gmail.com>
2021-05-28 22:11:11 +0700

    tut

--- 27-05-2021/tim_gia_tri_lon_nhat.js
+++ 27-05-2021/tim_gia_tri_lon_nhat.js
@@ -1,2 +1,7 @@
 //tính tích các phần tử của mảng sau
 var arr = [99, 69, 96, 24, 36, 98, 101, 198, 20, 16];
+var tich= arr[0];
+for (var i=1; i < arr.length; i++) {
+    tich = tich*arr[i]
+}
+console.log(tich)
--- 27-05-2021/tim_so_chan_lon_nhat.js
+++ 27-05-2021/tim_so_chan_lon_nhat.js
@@ -1,2 +1,15 @@
 // tìm và in ra số chẵn lớn nhất trong mảng sau
 var arr = [2, 76, 98, 99, 201, 202, 368, 401, 569];
+sochan = []
+for (var i=0; i < arr.length; i++) {
+  if(arr[i] % 2 === 0) {
+    sochan.push(arr[i])
+  }
+}
+var max = sochan[0]
+for (var j=0; j< sochan.length; j++) {
+  if(max < sochan[j]){
+    max = sochan[j]
+  }
+}
+console.log(max)
